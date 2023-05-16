f84fc2cfffb07c8bd87350c04a3e3682e5b226f2
Andreas Calleja <andreascalleja@gmail.com>
2023-05-16 21:26:48 +0200

    Registering users

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
+from . import db
 
 auth = Blueprint("auth", __name__)
 
@@ -34,7 +35,14 @@ def sign_up():
         elif len(password1) < 7:
             flash("Passowrds must be at least 8 characters", category="error")
         else:
-            new_user = User(email=email, first_name=first_name)
+            new_user = User(
+                email=email,
+                first_name=first_name,
+                password=generate_password_hash(password1, method="sha256"),
+            )
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account created", category="success")
+            return redirect(url_for("views.home"))
 
     return render_template("sign_up.html")
