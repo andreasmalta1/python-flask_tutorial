f57a88b30fec9aa0151f090f67408ebd54604aaf
Andreas Calleja <andreascalleja@gmail.com>
2023-05-16 21:35:07 +0200

    Added user login

--- website/auth.py
+++ website/auth.py
@@ -2,14 +2,28 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    data = request.form
-    print(data)
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in succesfully", category="success")
+                login_user(user, remember=True)
+                return redirect(url_for("views.home"))
+            else:
+                flash("Incorrect password", category="error")
+        else:
+            flash("Email does not exist", category="error")
+
     return render_template("login.html", text="Testing")
 
 
@@ -26,7 +40,10 @@ def sign_up():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email already exists", category="error")
+        elif len(email) < 4:
             flash("Email must be greater than 3 characters", category="error")
         elif len(first_name) < 2:
             flash("First name must be greater than 1 character", category="error")
@@ -43,6 +60,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash("Account created", category="success")
+            login_user(user, remember=True)
             return redirect(url_for("views.home"))
 
     return render_template("sign_up.html")
